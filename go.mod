3ca39c963ae520d6c17d2f76f6955ff8f72b4da6
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2020-04-21 12:52:57 +0200

    bump(*)

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/p0lyn0mial/failure-detector
+
+go 1.13
+
+replace github.com/p0lyn0mial/batch-working-queue => ../batch-working-queue
+
+replace github.com/p0lyn0mial/ttl-cache => ../ttl-cache
+
+require (
+	github.com/p0lyn0mial/batch-working-queue v0.0.0-00010101000000-000000000000
+	github.com/p0lyn0mial/ttl-cache v0.0.0-00010101000000-000000000000
+	k8s.io/apimachinery v0.18.1
+)
---
+++ vendor/github.com/p0lyn0mial/batch-working-queue/batch_working_queue.go
@@ -0,0 +1,78 @@
+package batch_working_queue
+
+import (
+	"sync"
+
+	"k8s.io/apimachinery/pkg/util/sets"
+)
+
+type keyFunc func(obj interface{}) string
+
+type Queue struct {
+	keyFn keyFunc
+	lock  sync.Mutex
+
+	store      map[string][]interface{}
+	dirty      map[string][]interface{}
+	inProgress sets.String
+
+	q []string
+}
+
+func New(keyFn func(obj interface{}) string) *Queue {
+	return &Queue{
+		keyFn:      keyFn,
+		store:      map[string][]interface{}{},
+		dirty:      map[string][]interface{}{},
+		inProgress: sets.NewString(),
+	}
+}
+
+func (q *Queue) Add(item interface{}) {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+	key := q.keyFn(item)
+	if q.inProgress.Has(key) {
+		q.dirty[key] = append(q.dirty[key], item)
+		return
+	}
+	if len(q.store[key]) == 0 {
+		q.q = append(q.q, key)
+	}
+	q.store[key] = append(q.store[key], item)
+}
+
+func (q *Queue) Get() (key string, items []interface{}) {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+	if len(q.q) == 0 {
+		// TODO: block until we have something in Queue ?
+		return "", nil
+	}
+	workKey := q.q[0]
+	q.q = q.q[1:]
+	work := q.store[workKey]
+	q.store[workKey] = []interface{}{}
+	q.inProgress.Insert(workKey)
+
+	return workKey, work
+}
+
+func (q *Queue) Done(key string) {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+
+	if !q.inProgress.Has(key) {
+		return
+	}
+	if len(q.dirty[key]) == 0 {
+		q.inProgress.Delete(key)
+		return
+	}
+
+	q.store[key] = q.dirty[key]
+	delete(q.dirty, key)
+	q.q = append(q.q, key)
+	q.inProgress.Delete(key)
+}
+
